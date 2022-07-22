d7e994b5c7604fbdd016ff728584fad6886eb8ea
LeeeeHo02 <a04075725@gmail.com>
2022-07-22 19:48:43 +0900

    middleware이란

--- src/server.js
+++ src/server.js
@@ -4,16 +4,37 @@ const PORT = 4000;
 //express어플리케이션 생성
 const app = express();
 
-const handleHome = (req,res) => {
-  console.log(res.send);
-  return res.send("Home");
-}
-const handleLogin = (req, res) => {
-  return res.send("Login");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 }
 
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed!");
+  next();
+}
+
+const handleHome = (req,res) => {
+  console.log(res.send);
+  return res.send("<h1>Home</h1>");
+}
+const handleLogin = (req, res) => {
+  return res.send({ message : "Login"});
+}
+
+const handleProtected = (req, res) => {
+  return res.send("welcome to the private lounge");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 const handleListening = () => console.log("Server listening on port 4000!!🚀");
 
 app.listen(PORT, handleListening);
